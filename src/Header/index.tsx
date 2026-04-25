f4237e248103e5e9b621ee747a366356b37251fe
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-04-25 09:38:43 -0300

    Header layout refactor

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 export default function Header() {
     return (
         <header>
-            <div className='dsf-header-container'>
+            <div className='dsf-header-container container'>
                 <h1 className='dsf-header-title'>DSFilter</h1>
                 <p className='dsf-header-counter'>6 produto(s)</p>
             </div>
