9a6b2f85e61a7064135c3199dd062cc0b9fcf586
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-04-25 09:17:38 -0300

    Header layout

---
+++ src/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header() {
+    return (
+        <header>
+            <div className='dsf-header-container'>
+                <h1 className='dsf-header-title'>DSFilter</h1>
+                <p className='dsf-header-counter'>6 produto(s)</p>
+            </div>
+        </header>
+    );
+}
